2afd29d3c9983fdcf53d88a03aa157cf94ebdf42
Lidd <logan.liddiard@yahoo.com>
2023-10-15 14:43:23 -0600

    Main page mostly done. Formatting and new font added. redid button design. Will come back to later for editting and formatting.

--- my-react-app/src/views/HomePage.tsx
+++ my-react-app/src/views/HomePage.tsx
@@ -11,7 +11,6 @@ export default function HomePage() {
       {/*Logo*/}
 
       <div className="centerFormat">
-
         <h1 className= "logo-fontFirst" style= {{
             padding: "1px",
             border: "1px",
@@ -23,11 +22,64 @@ export default function HomePage() {
             margin: "0px",
         }}>CONES</h1>
 
+<br></br>
 
+{/* sign in*/}
+      <h1 className="header-font">
+
+        Welcome to the future!!
+      </h1>
+
+
+      <Button style={{
+          backgroundColor: "purple",
+          fontFamily:"pixelfont"
+
+        }} variant="contained" component={Link} to="/login">
+        Log In!
+      </Button>
+
+
+{/*Sign up*/}
+      <p className="header-font">
+        New to Drone Cones? Sign up here!
+        </p>
+
+      <Button style={{
+          backgroundColor: "purple",
+          fontFamily:"pixelfont"
+          
+        }}  variant="contained" component={Link} to="/signup">
+        Sign Up
+      </Button>
       </div>
 
+{/*Introduction*/}
+
+<h1 className="header-font">Who are we?</h1>
+
+<p className = "pixel-font">We are Drone Cones! We're taking the joy of ice cream to new heights - quite literally! 
+  We are a small, innovative startup company dedicated to revolutionizing your ice cream cravings 
+  with our cutting-edge ice cream delivery service via drones. Gone are the days of impatiently waiting 
+  for your favorite frozen treats. With Drone Cones, we bring the ice cream parlor straight to your doorstep, 
+  soaring through the skies for a uniquely delightful delivery experience.</p>
+
+  <h1 className="header-font">Our Mission</h1>
+<h1 className = "pixel-font">Our mission is simple: to make every moment sweeter. Whether you're celebrating a special occasion, 
+  indulging in a late-night craving, or simply treating yourself, Drone Cones is here to elevate your 
+  ice cream experience. Our drones are not only fast and efficient but also eco-friendly, ensuring your 
+  treats arrive with a minimal carbon footprint.</h1>
 
 
+
+
+
+
+      <div>
+
+
+      </div>
+      
 
       <h1>Home Page, displays company info and links to Login and Signup</h1>
       <Button variant="contained" component={Link} to="/login">
