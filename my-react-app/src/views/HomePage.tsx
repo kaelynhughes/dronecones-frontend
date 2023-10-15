88aaf1287e1af6a6b57ebc405501d909e411e1d6
Lidd <logan.liddiard@yahoo.com>
2023-10-15 13:45:47 -0600

    Font add, font-family fixed, and formatting started *needs adjusting though*

--- my-react-app/src/views/HomePage.tsx
+++ my-react-app/src/views/HomePage.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Button from "@mui/material/Button";
+
 import { Link } from "react-router-dom";
+
+
 
 export default function HomePage() {
   return (
     <>
+      {/*Logo*/}
+
+      <div className="centerFormat">
+
+        <h1 className= "logo-fontFirst">DRONE</h1>
+        <h1 className= "logo-fontSecond">CONES</h1>
+
+
+      </div>
+
+
+
+
       <h1>Home Page, displays company info and links to Login and Signup</h1>
       <Button variant="contained" component={Link} to="/login">
         Log In
