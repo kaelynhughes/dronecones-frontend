738f70b8743103ffb435d333fd913b1287b6d77c
Lidd <logan.liddiard@yahoo.com>
2023-10-22 17:31:31 -0600

    Changes to top bar in app and added fonts to menu

--- my-react-app/src/views/customer/MenuPage.tsx
+++ my-react-app/src/views/customer/MenuPage.tsx
@@ -5,10 +5,9 @@ import { Link } from "react-router-dom";
 export default function MenuPage() {
   return (
     <>
-      <h1>
-        Menu Page, displays cone order options and lets user add cones to their
-        cart
-      </h1>
+      <h1 className="header-font">TOPPINGS</h1>
+      <h1 className="header-font">FLAVOR</h1>
+      <h1 className="header-font">CONE</h1>
       <Button variant="contained" component={Link} to="/app/cart">
         View Cart
       </Button>
