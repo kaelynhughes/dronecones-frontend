e896b48c267708d3c96842923efd7f7de573917d
Kaelyn Hughes <kaelynmhughes@gmail.com>
2023-10-28 11:57:00 -0600

    add dummy data to menu page: :

---
+++ my-react-app/src/services/useGetMenu.ts
@@ -0,0 +1,55 @@
+import axios from "./axios";
+import { BACKEND_URL_DEV } from "../constants";
+import { Product, ProductType } from "../types";
+
+export default () => {
+  axios
+    .get(`/customer/menu`, {
+      headers: { "Access-Request-Control-Origin": "*" },
+      transformRequest: [
+        function (data, headers) {
+          console.log("DATA");
+          console.log(data);
+          console.log("HEADERS");
+          console.log(headers);
+        },
+      ],
+    })
+    .then((response) => {
+      console.log(response.data);
+    });
+
+  const dummyData: Product[] = [
+    { name: "Sugar", stock: 100, ppu: 100, cpu: 50, type: ProductType.CONE },
+    { name: "Waffle", stock: 20, ppu: 200, cpu: 60, type: ProductType.CONE },
+    {
+      name: "Chocolate",
+      stock: 40,
+      ppu: 100,
+      cpu: 50,
+      type: ProductType.ICECREAM,
+    },
+    {
+      name: "Vanilla",
+      stock: 60,
+      ppu: 100,
+      cpu: 50,
+      type: ProductType.ICECREAM,
+    },
+    {
+      name: "Sprinkles",
+      stock: 300,
+      ppu: 10,
+      cpu: 5,
+      type: ProductType.TOPPING,
+    },
+    {
+      name: "Hot Fudge",
+      stock: 100,
+      ppu: 30,
+      cpu: 20,
+      type: ProductType.TOPPING,
+    },
+  ];
+  return dummyData;
+};
--- my-react-app/src/views/customer/MenuPage.tsx
+++ my-react-app/src/views/customer/MenuPage.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
+import useGetMenu from "../../services/useGetMenu";
+import { Product } from "../../types";
 
 const optionButtonStyle = {
   backgroundColor: "purple",
   fontFamily: "pixelfont",
   textShadow: "0 0 10px rgba(0, 153, 255, 0.7)",
-  color: "#ffff"
-
+  color: "#ffff",
 };
 
-{/*These constant variables should be instaciated from the database, for now these will be filled up based from defaults that I will set*/}
+{
+  /*These constant variables should be instaciated from the database, for now these will be filled up based from defaults that I will set*/
+}
 const toppingOption_1 = "Fudge";
 const toppingOption_2 = "M&Ms";
 const toppingOption_3 = "Oreos";
@@ -23,140 +26,186 @@ const cone_1 = "Cake";
 const cone_2 = "Sugar";
 const cone_3 = "Waffle";
 
-
-{/*0 indicates required element has not been selected*/}
-let toppings = ["","",""];
+{
+  /*0 indicates required element has not been selected*/
+}
+let toppings = ["", "", ""];
 let flavor = "";
 let cone = "";
 
-{/*Resets temporary variables when building a cone*/}
+{
+  /*Resets temporary variables when building a cone*/
+}
 function resetOptions() {
-  toppings = ["","",""];
+  toppings = ["", "", ""];
   flavor = "";
   cone = "";
 }
 
 export default function MenuPage() {
-  return (
+  const [menu, setMenu] = useState<Product[] | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchMenu = async () => {
+    const menuInfo = await useGetMenu();
+    console.log("MENU INFO");
+    console.log(menuInfo);
+    if (menuInfo instanceof Error) {
+      setError(menuInfo);
+    } else {
+      setMenu(menuInfo);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+  return error ? (
     <>
-
-{/*TOPPINGS*/}
-      <h1 className="header-font">TOPPINGS</h1>
+      <h1>ERROR</h1>
+    </>
+  ) : (
+    <>
+      {/*TOPPINGS*/}
+      <h1 className="header-font" onClick={useGetMenu}>
+        TOPPINGS
+      </h1>
       {/*ADD/TAKEOFF option 1 BUTTON*/}
-        <Button variant="contained"
+      <Button
+        variant="contained"
         sx={optionButtonStyle}
-        onClick={() =>{ 
-            if(toppings[0]== ""){
-              toppings[0] = toppingOption_1;
-              
-            }
-            else{
-              toppings[0] = "";
-            }
-            console.log(toppings[0])
-        
-        }}> {toppingOption_1} </Button>
-
+        onClick={() => {
+          if (toppings[0] == "") {
+            toppings[0] = toppingOption_1;
+          } else {
+            toppings[0] = "";
+          }
+          console.log(toppings[0]);
+        }}
+      >
+        {" "}
+        {toppingOption_1}{" "}
+      </Button>
       {/*ADD/TAKEOFF option 2 BUTTON*/}
-      <Button variant="contained"
+      <Button
+        variant="contained"
         sx={optionButtonStyle}
-        onClick={() =>{ 
-            if(toppings[1]== ""){
-              toppings[1] = toppingOption_2;
-            }
-            else{
-              toppings[1] = "";
-            }
-            console.log(toppings[1])
-        
-        }}> {toppingOption_2} </Button>
-
+        onClick={() => {
+          if (toppings[1] == "") {
+            toppings[1] = toppingOption_2;
+          } else {
+            toppings[1] = "";
+          }
+          console.log(toppings[1]);
+        }}
+      >
+        {" "}
+        {toppingOption_2}{" "}
+      </Button>
       {/*ADD/TAKEOFF option 3 BUTTON*/}
-      <Button variant="contained"
+      <Button
+        variant="contained"
         sx={optionButtonStyle}
-        onClick={() =>{ 
-            if(toppings[2]== ""){
-              toppings[2] = toppingOption_3;
-            }
-            else{
-              toppings[2] = "";
-            }
-            console.log(toppings[2])
-        
-        }}> {toppingOption_3} </Button>
-
-{/*FLAVORS*/}
+        onClick={() => {
+          if (toppings[2] == "") {
+            toppings[2] = toppingOption_3;
+          } else {
+            toppings[2] = "";
+          }
+          console.log(toppings[2]);
+        }}
+      >
+        {" "}
+        {toppingOption_3}{" "}
+      </Button>
+      {/*FLAVORS*/}
       <h1 className="header-font">FLAVOR</h1>
-            {/*ADD/TAKEOFF option 1 BUTTON*/}
-            <Button variant="contained"
-            sx={optionButtonStyle}
-        onClick={() =>{ 
-
-              flavor = flavor_1;
-              console.log(flavor);
-
-        
-        }}> {flavor_1} </Button>
-            {/*ADD/TAKEOFF option 2 BUTTON*/}
-        <Button variant="contained"
+      {/*ADD/TAKEOFF option 1 BUTTON*/}
+      <Button
+        variant="contained"
         sx={optionButtonStyle}
-          onClick={() =>{ 
-                flavor = flavor_2;
-                console.log(flavor);
-        
-        }}> {flavor_2} </Button>
-
-            {/*ADD/TAKEOFF option 2 BUTTON*/}
-            <Button variant="contained"
-            sx={optionButtonStyle}
-          onClick={() =>{ 
-                flavor = flavor_3;
-                console.log(flavor);
-        
-        }}> {flavor_3} </Button>
-
-{/*CONES*/}
+        onClick={() => {
+          flavor = flavor_1;
+          console.log(flavor);
+        }}
+      >
+        {" "}
+        {flavor_1}{" "}
+      </Button>
+      {/*ADD/TAKEOFF option 2 BUTTON*/}
+      <Button
+        variant="contained"
+        sx={optionButtonStyle}
+        onClick={() => {
+          flavor = flavor_2;
+          console.log(flavor);
+        }}
+      >
+        {" "}
+        {flavor_2}{" "}
+      </Button>
+      {/*ADD/TAKEOFF option 2 BUTTON*/}
+      <Button
+        variant="contained"
+        sx={optionButtonStyle}
+        onClick={() => {
+          flavor = flavor_3;
+          console.log(flavor);
+        }}
+      >
+        {" "}
+        {flavor_3}{" "}
+      </Button>
+      {/*CONES*/}
       <h1 className="header-font">CONE</h1>
-
-            {/*ADD/TAKEOFF option 1 BUTTON*/}
-            <Button variant="contained"
-            sx={optionButtonStyle}
-        onClick={() =>{ 
-              cone = cone_1;
-              console.log(cone);
-        
-        }}> {cone_1} </Button>
-
-          {/*ADD/TAKEOFF option 2 BUTTON*/}
-          <Button variant="contained"
-          sx={optionButtonStyle}
-      onClick={() =>{ 
-            cone = cone_2;
-            console.log(cone);
-      
-      }}> {cone_2} </Button>
-        
-                  {/*ADD/TAKEOFF option 3 BUTTON*/}
-                  <Button variant="contained"
-                  sx={optionButtonStyle}
-      onClick={() =>{ 
-            cone = cone_3;
-            console.log(cone);
-      
-      }}> {cone_3} </Button>
-
-
-{/*CART*/}
-<h1 className="header-font">CART</h1>
-
-{/*This button will post item to cart database and reset everything locally that has been pressed*/}
-<Button variant="contained"
-sx={optionButtonStyle}>
+      {/*ADD/TAKEOFF option 1 BUTTON*/}
+      <Button
+        variant="contained"
+        sx={optionButtonStyle}
+        onClick={() => {
+          cone = cone_1;
+          console.log(cone);
+        }}
+      >
+        {" "}
+        {cone_1}{" "}
+      </Button>
+      {/*ADD/TAKEOFF option 2 BUTTON*/}
+      <Button
+        variant="contained"
+        sx={optionButtonStyle}
+        onClick={() => {
+          cone = cone_2;
+          console.log(cone);
+        }}
+      >
+        {" "}
+        {cone_2}{" "}
+      </Button>
+      {/*ADD/TAKEOFF option 3 BUTTON*/}
+      <Button
+        variant="contained"
+        sx={optionButtonStyle}
+        onClick={() => {
+          cone = cone_3;
+          console.log(cone);
+        }}
+      >
+        {" "}
+        {cone_3}{" "}
+      </Button>
+      {/*CART*/}
+      <h1 className="header-font">CART</h1>
+      {/*This button will post item to cart database and reset everything locally that has been pressed*/}
+      <Button variant="contained" sx={optionButtonStyle}>
         Add to Cart
       </Button>
-
-      <Button variant="contained" component={Link} to="/app/cart"
-      sx={optionButtonStyle}>
+      <Button
+        variant="contained"
+        component={Link}
+        to="/app/cart"
+        sx={optionButtonStyle}
+      >
         View Cart
       </Button>
     </>
