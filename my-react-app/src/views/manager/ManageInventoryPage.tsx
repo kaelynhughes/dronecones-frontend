47ab9b5a6a7c9c3ff431606739ef7dbda10247af
Lidd <logan.liddiard@yahoo.com>
2023-11-10 14:07:03 -0700

    Can add items and app keeps track of input fields and radio button selection. Still some issues. Needs ID creation, Styiling for input form, small sorting bug.

--- my-react-app/src/views/manager/ManageInventoryPage.tsx
+++ my-react-app/src/views/manager/ManageInventoryPage.tsx
@@ -7,6 +7,11 @@ import { Product, UserType,ProductType } from "../../types";
 import { useStore } from "../../store";
 import useGetInventory from "../../services/manager/useGetInventory";
 import {
+  Radio,
+  FormLabel,
+  FormControlLabel,
+  FormControl,
+  RadioGroup,
   TextField,
   Box,
   Card,
@@ -37,11 +42,16 @@ const wordStyle = {
 export default function ManageInventoryPage() {
   const user = useStore((state) => state.user);
 
+  let productID = -1;
   const { editProduct } = useStore();
   const { removeProduct } = useStore();
-  
+  const { addProduct } = useStore();
 
   const [inventory, setInventory] = useState<Product[] | null>();
+  let [newProductName, setNewProductName] = useState("");
+  let [newProductQuantity, setNewProductQuantity] = useState(0);
+  let [newProductPrice, setNewPrice] = useState(0);
+  let [newProductType, setNewProductType] = useState<ProductType>(ProductType.ICECREAM);
 
   const fetchInventory = async () => {
     const fetchedInventory = await useGetInventory();
@@ -80,7 +90,7 @@ if (products.length === 0) {
     /*Populate topping options*/
   }
   const renderedToppingsList = toppingsList.map((item) => {
-    let localQuanitity = 69;
+    let localQuanitity = 0;
     return (
       <Card key={item.id}>
         <div style={{ display: "flex" }} >
@@ -112,7 +122,7 @@ if (products.length === 0) {
           onChange={(event) => {
             if (
               parseInt(event.target.value) <= 100 &&
-              parseInt(event.target.value) >= 0
+              parseInt(event.target.value) >= -100
             ) {
               localQuanitity = parseInt(event.target.value);
               console.log(localQuanitity);
@@ -146,7 +156,7 @@ if (products.length === 0) {
                       }
                       
                       editProduct(item.id,updateItem);
-                      console.log("YAY");
+                      
                     }
           }}  
       >
@@ -226,7 +236,7 @@ if (products.length === 0) {
                       }
                       
                       editProduct(item.id,updateItem);
-                      console.log("YAY");
+                    
                     }
           }}  
       >
@@ -271,11 +281,11 @@ if (products.length === 0) {
             error: errorState,
             helperText: "Invalid.",
           }}
-          //value={defaultValue}
+
           onChange={(event) => {
             if (
               parseInt(event.target.value) <= 100 &&
-              parseInt(event.target.value) >= 0
+              parseInt(event.target.value) >= -100
             ) {
               localQuanitity = parseInt(event.target.value);
               console.log(localQuanitity);
@@ -307,9 +317,7 @@ if (products.length === 0) {
                           updateItem.stock = 0
                         }
                       }
-                      
                       editProduct(item.id,updateItem);
-                      console.log("YAY");
                     }
           }}  
       >
@@ -333,19 +341,13 @@ if (products.length === 0) {
           Manage Inventory Page, displays remaining stock and inventory cost,
             allows user to bulk restock items, add new items, and change prices
           */}
-            
+
           <div style={{ display: "flex" }}>
+
+          {/* LHS Display and edit items */}
+          <div>
           <h1 className="header-font">Ice Cream</h1>
-          <IconButton
-          aria-label="delete"
-          color="secondary"
-          onClick={() => {
-                    products
-          }}  
-      >
-      <DeleteIcon />
-      </IconButton>
-          </div>
+          
           
           {renderedFlavorList}
           <h1 className="header-font">Toppings</h1>
@@ -359,7 +361,113 @@ if (products.length === 0) {
             to="/app/manager-quickview"
           >
             Back to Quickview
-          </Button>
+          </Button>            
+          </div>
+
+          {/* Add items */}
+          <div>
+          <Card
+            style={{
+              marginLeft: "100px",
+              paddingLeft: "100px",
+              paddingRight: "100px",
+            }}
+          >
+            <div className="centerFormat">
+              
+                <h1 style={{ margin: "10px" }} className="header-font">
+                  Add Product
+                </h1>
+                {/* Product name */}
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Product Name"
+                  defaultValue=""
+                  sx={{margin:"10px", fontFamily:"pixelfont"}}
+                  onChange={(event) => {
+                    newProductName = event.target.value;
+                  }}
+                />
+                {/* Quantity */}
+                <TextField 
+                    id="Outlined-required"
+                    label="Quantity"
+                    type="number"
+                    defaultValue="0"
+                    inputProps={{
+                      min: "0",
+                      max: "100",
+                      step: "10",
+                      error: errorState,
+                      helperText: "Invalid.",
+                    }}
+    
+                    onChange={(event) => {
+                      newProductQuantity = parseInt(event.target.value);
+                    }}
+                    sx={{margin:"10px", fontFamily:"pixelfont"}}
+                  />  
+                  {/* Price */}
+                  <TextField 
+                    id="Outlined-required"
+                    label="Pricing"
+                    type="number"
+                    defaultValue="1.00"
+                    inputProps={{
+                      min: "0",
+                      max: "100",
+                      step: ".5",
+                      error: errorState,
+                      helperText: "Invalid.",
+                    }}
+    
+                    onChange={(event) => {
+                      newProductPrice = parseFloat(event.target.value);
+                    }}
+                    sx={{margin:"10px", fontFamily:"pixelfont"}}
+                  />  
+                  {/* Product Type Radio Buttons */}
+                  <FormControl>
+                    <FormLabel id="demo-radio-buttons-group-label">Type</FormLabel>
+                    <RadioGroup
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      defaultValue=""
+                      name="radio-buttons-group"
+                    >
+                      <FormControlLabel value="flavor" control={<Radio />} label="Flavor" onChange={(event)=> {setNewProductType(ProductType.ICECREAM)}}/>
+                      <FormControlLabel value="topping" control={<Radio />} label="Topping" onChange={(event)=> {setNewProductType(ProductType.TOPPING)}}/>
+                      <FormControlLabel value="cone" control={<Radio />} label="Cone" onChange={(event)=> {setNewProductType(ProductType.CONE)}}/>
+                    </RadioGroup>
+                  </FormControl>
+                  <Button
+                  onClick={(event)=> {
+                    if(newProductName != "" && newProductPrice != 0){
+                      
+                      //ID SHOULD BE CREATED VIA API
+                      addProduct({ 
+                        name:newProductName,
+                        stock: newProductQuantity,
+                        ppu: newProductPrice,
+                        type: newProductType,
+                        id: 23
+                      });
+                      newProductName = "";
+                      newProductPrice = 0;
+                      newProductQuantity = 0;
+                      console.log("Item Added!")
+                    }
+                    else{ console.log("Missing Field...")};
+
+                  }}
+                  >
+                    Add Item
+                  </Button>
+              <br></br>
+            </div>
+          </Card>
+          </div>
+          </div>
         </>
       )}
     </>
