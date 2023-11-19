bf06b8791d9441ee4544a51cdfd7c0325430f7ac
Lidd <logan.liddiard@yahoo.com>
2023-11-19 16:44:38 -0700

    Simple history page with accordian views similar to drone history and similar information displayed

--- my-react-app/src/views/manager/ManagerHistoryPage.tsx
+++ my-react-app/src/views/manager/ManagerHistoryPage.tsx
@@ -4,6 +4,27 @@ import { Link } from "react-router-dom";
 import { Order, UserType } from "../../types";
 import { useStore } from "../../store";
 import useGetHistory from "../../services/manager/useGetHistory";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Accordion,
+  AccordionSummary,
+  Typography,
+  AccordionDetails,
+  useTheme,
+  Box,
+} from "@mui/material";
+import { getConeString, getPriceString } from "../../services/helperFunctions";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const wordStyle = {
+  color: "white",
+  fontSize: "16px",
+  fontFamily: "pixelfont",
+};
+
 
 export default function ManagerHistoryPage() {
   const user = useStore((state) => state.user);
@@ -13,6 +34,46 @@ export default function ManagerHistoryPage() {
     setHistory(fetchedHistory);
   };
 
+  console.log(history);
+
+  let renderedHistory = history?.map((order) => {
+
+    
+    return(
+      <Accordion
+      sx={{
+        width: "1130px",
+        "& .MuiTypography-root": wordStyle,
+        flexGrow: 1,
+      }}
+      key={order.id}
+    >
+      <AccordionSummary
+        sx={{ flexGrow: 1, width: "100%",fontFamily:"pixelfont",color:"white" }}
+        expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
+      >
+
+        {/*REPLACE CUSTOMER NAME LATER*/}
+          {order.timestamp.getMonth()}/{order.timestamp.getDate()}/{order.timestamp.getFullYear()} {order.timestamp.getHours()}:{order.timestamp.getMinutes()} - CUSTOMER NAME - Order Size: {order.cones.length}
+      </AccordionSummary>
+      <AccordionDetails sx={{ flexGrow: 1, width: "100%" }}>
+        {/*REPLACE DRONEID AND DRONEOPERATOR ID LATER*/}
+        DRONEID AND DRONEOPERATOR ID
+        {order.cones.map((cone, index) => (
+          <Typography key={index}>{getConeString(cone)}</Typography>
+        ))}
+        <Typography>{`Total Order Price: ${getPriceString(
+          order.totalPrice
+        )}`}</Typography>
+        <Typography>{`Employee cut: ${getPriceString(
+          order?.employeeCut || 0
+        )}`}</Typography>
+      </AccordionDetails>
+    </Accordion>
+      
+    );
+  })
+
   useEffect(() => {
     fetchHistory();
   }, []);
@@ -20,17 +81,15 @@ export default function ManagerHistoryPage() {
     <>
       {user.userType === UserType.MANAGER && (
         <>
-          <h1>
-            Manager History Page, allows the manager to view all recent orders
-            and their details
+
+        {/* Manager History Page, allows the manager to view all recent orders
+            and their details */}
+          <h1 className="header-font">
+            History
           </h1>
-          <Button
-            variant="contained"
-            component={Link}
-            to="/app/manager-quickview"
-          >
-            Back to Quickview
-          </Button>
+          
+          {renderedHistory}
+          
         </>
       )}
     </>
